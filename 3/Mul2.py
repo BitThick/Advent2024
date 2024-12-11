911a55cfe3099ea0a5eaf113dbea2019cc321366
Rob <rob@notrob.com>
2024-12-11 12:18:32 -0500

    new solution

--- 3/Mul2.py
+++ 3/Mul2.py
@@ -1,21 +1,20 @@
-#Name: Mul
-#Abstract: Find number pairs per a pattern, 
+#Name: Mul2
+#Abstract: Find number pairs per a pattern
+#          & between do() * don't(), then
 #          multiply them & sum the products
-
 
 import re
 
+sum = 0
 file = open('/home/user/Advent2024/3/Input.txt')
 text = file.read()
-list = re.findall("do\(\).+|\s*don't\(\)", text)
-for item in list:
-	print(item)
-input()
-sum = 0
-for item in list:
-	sub = item[4:-1]
-	numbers = sub.split(',')
-	total = int(numbers[0]) * int(numbers[1])
-	sum += total
+do = re.findall("do\(\)([\s\S]*?)don't\(\)", text)
+for item in do:
+	mul = re.findall('mul\(\d+,\d+\)', item)
+	for pair in mul:
+		sub = pair[4:-1]
+		numbers = sub.split(',')
+		total = int(numbers[0]) * int(numbers[1])
+		sum += total
 
 print(sum)
