fe5e7e1a36523319979ca9f9d6f3630ebebc1013
Rob <rob@notrob.com>
2024-12-11 15:35:43 -0500

    solution day 4 pt 1

--- 4/CrossWord.py
+++ 4/CrossWord.py
@@ -25,26 +25,37 @@ for line in lines:
 	for char in line:
 		charCount += 1
 		if char == 'X':
-			if (lineCount - 3) < 0 or (charCount - 3) < 0: continue
-			if lines[lineCount - 1][charCount - 1] == 'M':			#up-left diagonal
-				if lines[lineCount - 2][charCount - 2] == 'A':
-					if lines[lineCount - 3][charCount - 3] == 'S':
-						found += 1
-			if (lineCount - 3) < 0 or (charCount + 3) > len(line): continue
-			if lines[lineCount - 1][charCount + 1] == 'M':			#up-right diagonal
-				if lines[lineCount - 2][charCount + 2] == 'A':
-					if lines[lineCount - 3][charCount + 3] == 'S':
-						found += 1
-			if (lineCount + 3) > (len(lines) - 1) or (charCount + 3) > len(line): continue
-			if lines[lineCount + 1][charCount + 1] == 'M':			#down-right diagonal
-				if lines[lineCount + 2][charCount + 2] == 'A':
-					if lines[lineCount + 3][charCount + 3] == 'S':
-						found += 1
-			if (lineCount + 3) > (len(lines) - 1) or (charCount - 3) < 0: continue
-			if lines[lineCount + 1][charCount - 1] == 'M':			#down-left diagonal
-				if lines[lineCount + 2][charCount - 2] == 'A':
-					if lines[lineCount + 3][charCount - 3] == 'S':
-						found += 1
+			lineSize = len(line)
+			if (lineCount - 3) >= 0 and (charCount - 3) >= 0:
+				if lines[lineCount - 1][charCount - 1] == 'M':			#up-left diagonal
+					if lines[lineCount - 2][charCount - 2] == 'A':
+						if lines[lineCount - 3][charCount - 3] == 'S':
+							found += 1
+			if (lineCount - 3) >= 0 and (charCount + 3) < lineSize:
+				if lines[lineCount - 1][charCount + 1] == 'M':			#up-right diagonal
+					if lines[lineCount - 2][charCount + 2] == 'A':
+						if lines[lineCount - 3][charCount + 3] == 'S':
+							found += 1
+			if (lineCount + 3) < len(lines) and (charCount + 3) < lineSize:
+				if lines[lineCount + 1][charCount + 1] == 'M':			#down-right diagonal
+					if lines[lineCount + 2][charCount + 2] == 'A':
+						if lines[lineCount + 3][charCount + 3] == 'S':
+							found += 1
+			if (lineCount + 3) < len(lines) and (charCount - 3) >= 0:
+				if lines[lineCount + 1][charCount - 1] == 'M':			#down-left diagonal
+					if lines[lineCount + 2][charCount - 2] == 'A':
+						if lines[lineCount + 3][charCount - 3] == 'S':
+							found += 1
+			if (lineCount + 3) < len(lines):
+				if lines[lineCount + 1][charCount] == 'M':			#down
+					if lines[lineCount + 2][charCount] == 'A':
+						if lines[lineCount + 3][charCount] == 'S':
+							found += 1
+			if (lineCount - 3) >= 0:
+				if lines[lineCount -1][charCount] == 'M':			#up
+					if lines[lineCount - 2][charCount] == 'A':
+						if lines[lineCount - 3][charCount] == 'S':
+							found += 1
 			if line[charCount - 1] == 'M':							#backwards (tested good)
 				if line[charCount - 2] == 'A':
 					if line[charCount - 3] == 'S':
@@ -55,4 +66,3 @@ for line in lines:
 						found += 1
 
 print(found)
-#2058 is too low
